c89cff0229802006de62c64135af657eaae4c3ad
Om Patil <omajitsinhpatil@gmail.com>
2023-05-28 23:23:49 +0530

    api

---
+++ api/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const admin = require("firebase-admin");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvd0aSq5baLfm7znloTe8VaCnN8Rz-YPE",
+  authDomain: "pharmacymanagement-59505.firebaseapp.com",
+  databaseURL: "https://pharmacymanagement-59505-default-rtdb.firebaseio.com",
+  projectId: "pharmacymanagement-59505",
+  storageBucket: "pharmacymanagement-59505.appspot.com",
+  messagingSenderId: "579613358388",
+  appId: "1:579613358388:web:802827035c787280e58d61",
+  measurementId: "G-YS9LE6J4CQ",
+};
+
+const port = 3005;
+app.use(express.json());
+admin.initializeApp(firebaseConfig);
+const db = admin.firestore();
+const usercol = db.collection("Pharmacy");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+//basic complete data route
+app.get("/getMedicines", async (req, res) => {
+  try {
+    const usersSnapshot = await usercol.get();
+    const users = [];
+    usersSnapshot.forEach((doc) => {
+      users.push(doc.data());
+    });
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
